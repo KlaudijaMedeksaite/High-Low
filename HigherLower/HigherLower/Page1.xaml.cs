cdd27135af6d4be8389e33d5409887e5b9cf70c9
Klaudija Medeksaite <G00353761@gmit.ie>
2018-11-21 10:36:23 +0000

    fully functional game, no score yet, more gui to do

--- HigherLower/HigherLower/Page1.xaml.cs
+++ HigherLower/HigherLower/Page1.xaml.cs
@@ -14,7 +14,48 @@ namespace HigherLower
 	{
 		public Page1 ()
 		{
-			InitializeComponent ();
-		}
-	}
+            InitializeComponent ();
+            randomNumber();
+        }
+
+        private void higherBtn_Clicked(object sender, EventArgs e)
+        {
+            int lastNum = int.Parse(numDisplay.Text);
+
+            randomNumber();
+            int newNum = int.Parse(numDisplay.Text);
+
+            if (newNum > lastNum)
+            {
+                statusLbl.Text = "You win";
+            }
+            else
+            {
+                statusLbl.Text = "You lose";
+            }
+        }
+
+        private void lowerBtn_Clicked(object sender, EventArgs e)
+        {
+            int lastNum = int.Parse(numDisplay.Text);
+
+            randomNumber();
+            int newNum = int.Parse(numDisplay.Text);
+
+            if(newNum < lastNum)
+            {
+                statusLbl.Text = "You win";
+            }
+            else
+            {
+                statusLbl.Text = "You lose";
+            }
+        }
+        private void randomNumber()
+        {
+            //random number
+            Random generator = new Random();
+            numDisplay.Text = generator.Next(0, 21).ToString();
+        }
+    }
 }
